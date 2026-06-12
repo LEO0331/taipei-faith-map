4976818d84a91801ba4fbe80c78e0f14be426804
leo <25032781+LEO0331@users.noreply.github.com>
2026-06-12 16:58:34 +0800

    add deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/taipei-faith-map/' : '/',
   plugins: [react()],
 });
